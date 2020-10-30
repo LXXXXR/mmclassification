58e3f75ed490fd63366404d067dd8cfe16909564
lixinran <SENSETIME\lixinran@cn0014004120l.domain.sensetime.com>
2020-10-30 13:39:05 +0800

    change schedules config

--- configs/_base_/schedules/cifar10_bs128_mine.py
+++ configs/_base_/schedules/cifar10_bs128_mine.py
@@ -1,6 +1,9 @@
 # optimizer
-optimizer = dict(type='SGD', lr=1e-3, momentum=0.9, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=1e-3, momentum=0.9, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=1e-3, momentum=0)
+#optimizer = dict(type='RMSprop', lr=1e-3, alpha=0.99)
+optimizer = dict(type='Adam',lr=5e-4, betas=(0.9,0.99))
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=[100, 150])
-runner = dict(type='EpochBasedRunner', max_epochs=200)
+runner = dict(type='EpochBasedRunner', max_epochs=10)

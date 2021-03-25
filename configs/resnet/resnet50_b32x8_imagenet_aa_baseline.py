e1f4ebb6a5f97adc25fb0868154a4f069b31a40d
lixinran <lixr423@outlook.com>
2021-03-25 14:07:15 +0800

    fix

--- configs/resnet/resnet50_b32x8_imagenet_aa_baseline.py
+++ configs/resnet/resnet50_b32x8_imagenet_aa_baseline.py
@@ -9,5 +9,5 @@ data = dict(samples_per_gpu=64)
 optimizer = dict(type='SGD', lr=0.2, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='CosineAnnealing', min_lr=0)
+lr_config = dict(_delete_=True, policy='CosineAnnealing', min_lr=0)
 runner = dict(type='EpochBasedRunner', max_epochs=120)
